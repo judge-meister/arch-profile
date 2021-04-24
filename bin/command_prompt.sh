aca44a3a88a6b85975b3d9ab8a879fe919ea94c4
Ian Judge <ianjudge1969@gmail.com>
2021-04-24 22:20:32 +0100

    refactored prompt code

--- bin/command_prompt.sh
+++ bin/command_prompt.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+#
 # Command Prompt
 #
-# use param 0 or blank for normal colours
-# use param 1 for bold colours
 #
+# shellcheck disable=SC2034
 
 # colours
 
@@ -20,75 +21,95 @@ RED='\e[01;31m'
 GREEN='\e[01;32m'
 YELLOW='\e[01;33m'
 BLUE='\e[01;34m'
-PINK=='\e[01;35m'
+PINK='\e[01;35m'
 LIGHTCYAN='\e[01;36m'
-WHITE=='\e[01;37m'
+WHITE='\e[01;37m'
 
 NOCOL='\e[00m'
 
-# command_prompt
+# function pinched from http://github.com/jessfraz/dotfiles/.bash_prompt
+prompt_git() {
+	local s='';
+	local branchName='';
 
-# darkred time and date
-_0_d="\[${DARKRED}\]\d \t "
-_1_d="\[${RED}\]\d \t "
+	# Check if the current directory is in a Git repository.
+	if [ "$(git rev-parse --is-inside-work-tree &>/dev/null; echo "${?}")" == '0' ]; then
 
-# brown [ darkgreen user@host
-_0_u_h="\[${BROWN}\][\[${DARKGREEN}\]\u\[${DARKGREEN}\]@\[${NOCOL}\]\[${DARKGREEN}\]\h\[${NOCOL}\]"
-_1_u_h="\[${YELLOW}\][\[${GREEN}\]\u\[${GREEN}\]@\[${NOCOL}\]\[${GREEN}\]\h\[${NOCOL}\]"
+		# check if the current directory is in .git before running git checks
+		if [ "$(git rev-parse --is-inside-git-dir 2> /dev/null)" == 'false' ]; then
 
-#  cyan working dir  
-_0_w=" \[${CYAN}\]\w\[${NOCOL}\]"
-_1_w=" \[${BLUE}\]\w\[${NOCOL}\]"
+			if [[ -O "$(git rev-parse --show-toplevel)/.git/index" ]]; then
+				git update-index --really-refresh -q &> /dev/null;
+			fi;
 
-# gitps1 brown ]  
-_0_g2="\[${BROWN}\]]"
-_1_g2="\[${YELLOW}\]]"
+			# Check for uncommitted changes in the index.
+			if ! git diff --quiet --ignore-submodules --cached; then
+				s+='+';
+			fi;
 
-# newline brown [command number]
-_0_nl="\n\[${BROWN}\][\!]\[${NOCOL}\]"
-_1_nl="\n\[${YELLOW}\][\!]\[${NOCOL}\]"
+			# Check for unstaged changes.
+			if ! git diff-files --quiet --ignore-submodules --; then
+				s+='!';
+			fi;
+
+			# Check for untracked files.
+			if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+				s+='?';
+			fi;
+
+			# Check for stashed files.
+			if git rev-parse --verify refs/stash &>/dev/null; then
+				s+='$';
+			fi;
+
+		fi;
+
+		# Get the short symbolic ref.
+		# If HEAD isn’t a symbolic ref, get the short SHA for the latest commit
+		# Otherwise, just give up.
+		branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
+			git rev-parse --short HEAD 2> /dev/null || \
+			echo '(unknown)')";
+
+		[ -n "${s}" ] && s=" [${s}]";
+
+		echo -e "${1}${branchName}${BLUE}${s}";
+	else
+		return;
+	fi;
+}
+
+
+# command_prompt components
+
+# red time and date
+__date__="\[${RED}\]\d \t "
+
+# green user@host
+__user_host__="\[${DARKGREEN}\]\u\[${DARKGREEN}\]@\[${DARKGREEN}\]\h\[${NOCOL}\]"
+
+# cyan working dir  
+__working_dir__=" \[${BLUE}\]\w\[${NOCOL}\]"
+
+# yellow open and close bracket
+__close_bracket__="\[${YELLOW}\]] "
+__open_bracket__="\[${YELLOW}\]["
+
+# yellow [command number]
+__comm_num__="\[${YELLOW}\][\!]\[${NOCOL}\]"
 
 # add final $
 __end=" \$ "
 
-# Mercurial Functions
-hg_ps1()
-{
-    hg identify -b 2> /dev/null | sed -E s/^\(.+\)$/\ \(\\1\)/
-}
-hg_dirty() {
-    hg status 2> /dev/null \
-    | awk '$1 == "?" { print " untracked" } $1 != "?" { print " modified" }' \
-    | sort | uniq | head -1
-}
-hg_in_repo() {
-    [[ `hg branch 2> /dev/null` ]] && echo '[hg]'
-}
-function parse_git_remote {
-  echo -n $(git status 2>/dev/null | awk -v out=$1 '/# Your branch is / { if(out=="") print $5; else print out }')
-}
-# Git functions
-git_ps1()
-{
-    git branch 2> /dev/null | sed -E s/^\\*\ \(.+\)$/\ \(\\1\)/
-}
-function git_dirty {
-  echo -n ' '$(git status 2>/dev/null | awk -v out=$1 -v std="dirty" '{ if ($0=="# Changes to be committed:") std = "uncommited"; last=$0 } END{ if(last!="" && last!="nothing to commit (working directory clean)") { if(out!="") print out; else print std } }')
-}
-git_in_repo() {
-    [[ `git branch 2> /dev/null` ]] && echo ' [git]'
-}
-function git_remote {
-  echo -n $(git status 2>/dev/null | awk -v out=$1 '/# Your branch is / { if(out=="") print "("$4")"; else print out }')
-}
+# construct PS1
 
-
-#PS1=$__d$__u_h$__w'$(git_in_repo)$(git_ps1)$(git_dirty)$(git_remote)$(hg_in_repo)$(hg_ps1)$(hg_dirty)'$__g2$__nl$__end
-
-PS1=$_0_d$_0_u_h$_0_w$_0_g2$_0_nl$__end
-
-if [ $1 -eq 1 ]
-then
-  PS1=$_1_d$_1_u_h$_1_w$_1_g2$_1_nl$__end
-fi
+PS1="$__date__"
+PS1+="$__open_bracket__"
+PS1+="$__user_host__"
+PS1+="$__working_dir__"
+PS1+="$__close_bracket__"
+PS1+="\$(prompt_git \"\[${BROWN}\]\")"
+PS1+="\n"
+PS1+="$__comm_num__"
+PS1+="$__end"
 
