81ebf9ed8d6514e11bc0d7f2a4be3fa9e9c97926
Ian Judge <ianjudge1969@gmail.com>
2021-08-21 21:48:01 +0100

    wm config updates

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -7,7 +7,8 @@ VENDOR="$(cat /sys/class/dmi/id/sys_vendor)"
 if [ "$VENDOR" == "LENOVO" ]; 
 then
     setxkbmap -layout gb ;
-    ~/.fehbg & #nitrogen --restore &
+    ~/.fehbg & 
+    #nitrogen --restore &
 elif [ "$VENDOR" == "Apple Inc." ]; 
 then
     setxkbmap -layout gb -model macbook79 ;
