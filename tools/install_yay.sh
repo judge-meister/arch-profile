00d37306cded192238282945858235a5f9ea98e6
Ian Judge <ianjudge1969@gmail.com>
2021-10-26 23:24:24 +0100

    updates after testing

--- tools/install_yay.sh
+++ tools/install_yay.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # script to install yay
 
 cd /opt
 sudo git clone https://aur.archlinux.org/yay.git
-sudo chown -R judge:users ./yay
+sudo chown -R judge:judge /opt/yay
 cd yay
 makepkg -si
 
--- tools/install_yay_pkgs.sh
+++ tools/install_yay_pkgs.sh
@@ -5,5 +5,5 @@
 
 #sudo pacman -S $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
 
-sudo yay -S $(comm -12 <(yay -Sql | sort) <(sort yay_pkglist.txt | grep -v '^#' | awk -F' ' '{print $1}') )
+yay -S $(comm -12 <(yay -Sql | sort) <(sort yay_pkglist.txt | grep -v '^#' | awk -F' ' '{print $1}') )
 
