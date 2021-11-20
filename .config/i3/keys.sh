accc6cc1f86aca9fd8566d6669744f4d9ba6ccd4
Ian Judge <ianjudge1969@gmail.com>
2021-11-20 22:45:12 +0000

    fixed touchpad and added help screen

---
+++ .config/i3/keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+grep -E '^bind|# KP_GROUP' .config/i3/config  | \
+  grep -v -E  'workspace|Print ' | \
+  sed 's|bindsym $mod+|Mod-|g;s|bindsym ||g;s|# KP_GROUP\(.*\)|\n\1|g' | \
+  yad --text-info --back=#282c34 --fore=#46d9ff --geometry=1000x600
+
