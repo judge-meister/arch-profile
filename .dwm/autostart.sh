f7ffb047a6011cdfb1c332873a4adc828861d861
Ian Judge <ianjudge1969@gmail.com>
2021-11-26 17:46:33 +0000

    fixing dwm startup

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,17 +2,21 @@
 set -x
 
 VENDOR="$(cat /sys/class/dmi/id/sys_vendor)"
+PRODUCT="$(cat /sys/class/dmi/id/product_name)"
 
 
-if [ "$VENDOR" == "LENOVO" ]; 
+
+if [ "$PRODUCT" == "ThinkPad X230" ]; #if [ "$VENDOR" == "LENOVO" ]; 
 then
     setxkbmap -layout gb ;
     ~/.fehbg & 
     #nitrogen --restore &
-elif [ "$VENDOR" == "Apple Inc." ]; 
+ 
+elif [ "$PRODUCT" == "MacBookPro5,5" ]; #elif [ "$VENDOR" == "Apple Inc." ];
 then
     setxkbmap -layout gb -model macbook79 ;
-    "$HOME"/bin/macfnmode fn ;
+    ~/.fehbg & 
+    "$HOME"/bin/macfnmode media ;
 fi ;
 
 lxsession &
@@ -21,5 +25,5 @@ dwmblocks &
 
 # current config breaks F1 F2 f5 F6 F10 F11 F12 in order to get MacBook controls to sort of work
 # need to find another way, may be vendor specific config files
-xbindkeys
+xbindkeys &
 
