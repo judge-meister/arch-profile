b7583f5e1cf3e71ae50a1b0f3a50b651a059b385
Ian Judge <ianjudge1969@gmail.com>
2021-11-28 16:42:44 +0000

    new temp for macbook

--- .config/i3blocks/cpu_temp.sh
+++ .config/i3blocks/cpu_temp.sh
@@ -5,7 +5,8 @@ VENDOR=$(cat /sys/class/dmi/id/sys_vendor)
 
 if [ "$VENDOR" == "Apple Inc." ]
 then
-  TEMP_INPUT="/sys/class/hwmon/hwmon3/temp2_input"
+  #TEMP_INPUT="/sys/class/hwmon/hwmon3/temp2_input"
+  TEMP_INPUT="/sys/class/hwmon/hwmon1/temp1_input"
   t=$(cat $TEMP_INPUT); 
   echo "$t / 1000" | bc -s | awk '{print " "$1"°C"}'; 
 
@@ -16,7 +17,7 @@ then
 
 elif [ "$VENDOR" == "LENOVO" ]
 then
-  #TEMP_INPUT="/sys/class/hwmon/hwmon4/temp2_input"
+  #TEMP_INPUT="/sys/class/hwmon/hwmon1/temp1_input"
   TEMP=$(acpi -t | awk -F' ' '{print $4}' | sed 's/.0//g')
   echo " ${TEMP}°C"
   if [ $((TEMP)) -gt 80 ]
