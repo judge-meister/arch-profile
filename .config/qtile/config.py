75a78a036f5fc16fd6d0b642336d7f70694f1815
Ian Judge <ianjudge1969@gmail.com>
2022-05-07 18:57:49 +0100

    minor updates to configs

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -84,7 +84,7 @@ keys = [
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
 
     Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"),
 
