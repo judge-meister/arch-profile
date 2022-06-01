4feb8ae6c2415630453b3a527d3c883e942da91e
Ian Judge <ianjudge1969@gmail.com>
2022-06-01 23:51:40 +0100

    fixed shellcheck errors

--- bin/find_dependencies.sh
+++ bin/find_dependencies.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #set -x
-cd ~/
+cd ~/ || exit
 
-for x in  $( grep -E '# Requires:|// Requires:' $HOME/bin/* $HOME/.config/*/* $HOME/.local/bin/* 2>/dev/null \
+# shellcheck disable=SC2013
+for x in  $( grep -E '# Requires:|// Requires:' "$HOME"/bin/* "$HOME"/.config/*/* "$HOME"/.local/bin/* 2>/dev/null \
      | grep -v 'find_dependencies.sh' \
      | awk -F':' '{print $3}' \
      | sed 's|,||g;s|(optional)||g' )
 do 
-  echo $x
+  echo "$x"
 done | sort -u
  
