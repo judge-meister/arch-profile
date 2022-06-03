d5e1f6610073fac2d1a1e6f9998ec8e85998115c
Ian Judge <ianjudge1969@gmail.com>
2022-06-03 22:52:30 +0100

    script updates

--- bin/dim_wallpaper.sh
+++ bin/dim_wallpaper.sh
@@ -2,22 +2,35 @@
 set -x
 pictures=/home/judge/Pictures
 
-dimensions=1366x768
 opacity=0.65
 shade=out.png
 input=sexy-model-woman-boobs-nude-gorgeous-trimmed-pussy-african-pierced-nipples-1366x768.png 
+dimensions=$(identify "$input" |sed  's/.* \([0-9x]*\) .*/\1/g')
 result=result.png
-rm -f $shade $result
 
-# create a black image with opacity
-convert -size $dimensions xc:'rgba(0, 0, 0, '$opacity')' $shade
+dimmer()
+{
+  rm -f $shade $result
 
-# overlay the transparent image onto the wallpaper image
-#convert $input -gravity center $shade -composite $result
-composite $pictures/$input $shade $result
+  # create a black image with opacity
+  convert -size "$dimensions" xc:'rgba(0, 0, 0, '$opacity')' $shade
 
-rm -f $shade
+  # overlay the transparent image onto the wallpaper image
+  #convert $input -gravity center $shade -composite $result
+  composite $shade $pictures/$input $result
 
-pkill swaybg
-swaybg -m fit -i $pictures/$result 2>/dev/null &
+  #rm -f $shade
 
+  pkill swaybg
+  swaybg -m fit -i $pictures/$result 2>/dev/null &
+}
+
+if [ "$#" -eq 2 ]
+then
+    opacity="$1"
+    input="$2"
+    dimensions=$(identify "$input" |sed  's/.* \([0-9x]*\) .*/\1/g')
+    dimmer
+else
+    dimmer
+fi
